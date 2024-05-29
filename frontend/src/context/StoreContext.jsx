e4d83c09a1ef3632d2b738b24d474bfac471cfba
webdesign1507 <webdesign1507@gmail.com>
2024-05-29 10:56:49 +0530

    first commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StorecontextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:3000"
+  const url = "https://food-hotel-mern-backend.onrender.com"
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
